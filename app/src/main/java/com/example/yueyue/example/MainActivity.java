fdfbc6b63b8640fbd4594835dd33446198b2cec4
YueYue <1298375722@qq.com>
2016-05-27 21:51:22 +0800

    写了一个button 的东西

--- app/src/main/java/com/example/yueyue/example/MainActivity.java
+++ app/src/main/java/com/example/yueyue/example/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yueyue.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
@@ -14,5 +15,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         //二楼写了一个button的方法
         TextView textView;
+        Button button;
     }
 }
