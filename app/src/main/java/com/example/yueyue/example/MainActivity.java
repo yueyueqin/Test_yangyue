cb784ad2c20c21987779c408c8f915e98e2d2d00
YueYue <1298375722@qq.com>
2016-05-27 21:44:37 +0800

    这是第一个人提交的代码

--- app/src/main/java/com/example/yueyue/example/MainActivity.java
+++ app/src/main/java/com/example/yueyue/example/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.yueyue.example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button;
     }
 }
